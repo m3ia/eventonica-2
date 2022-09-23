27e65051a5b0181ee3e98802f0e8c6b5dc3c874d
m3ia <meianatividad@gmail.com>
2022-09-22 19:25:57 -0700

    Add Edit Users functionality

--- server/server.js
+++ server/server.js
@@ -17,17 +17,6 @@ app.listen(PORT, () => console.log(`Hola this server is running on port ${PORT}`
 
 app.get('/', (req, res) => res.json('Hello index'));
 
-// Get Mock Users
-const mockUsers = [
-  { name: "Marlin", email: "marlin@gmail.com", id: "1" },
-  { name: "Nemo", email: "nemo@gmail.com", id: "2" },
-  { name: "Dory", email: "dory@gmail.com", id: "3" }
-]
-
-// app.get('/users', function (req, res, next) {
-//   res.json({ users: mockUsers });
-// });
-
 app.get('/users', async function (req, res, next) {
 
   try {
